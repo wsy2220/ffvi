6ea8db0e210fe5f07a04906c735aac8c28cdbf4b
wsy <wsy2220@gmail.com>
2015-06-18 22:10:08 +0800

    add a header

---
+++ ffvi.h
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <libavutil/opt.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/channel_layout.h>
+#include <libavutil/common.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/mathematics.h>
+#include <libavutil/samplefmt.h>
+int ff_init(const char *filename, 
+		    int codec_id, 
+			int width, 
+			int height, 
+			int bit_rate,
+			int fps,
+			int pix_fmt);
+int ff_wframe(void *frame_raw);
+int ff_close();
